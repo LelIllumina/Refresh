89a7b742c6ad5f2a3dc2ab8f11b949e7ce35213d
Lel Illumina <dragonbilal938@outlook.com>
2025-07-23 23:28:40 +0500

    feat: add the integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import partytown from "@astrojs/partytown";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://yourSiteHere.nekoweb.org",
-  output: "static",
+
   //   server: { host: true }, // uncomment if live server dosent work for some reason
   // You can find more informaation about Astro's configuration options at https://docs.astro.build/en/reference/configuration-reference/
+  output: "static",
+
+  integrations: [partytown(), mdx(), sitemap()]
 });
