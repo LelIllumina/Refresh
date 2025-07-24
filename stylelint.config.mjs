1bd70243f6f2f92582e1d41ee48eebf4b9157674
Lel Illumina <dragonbilal938@outlook.com>
2025-07-24 07:41:39 +0500

    feat: stylelint config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,14 +1,22 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: [
-    "stylelint-config-html/astro",
     "stylelint-config-standard",
+    "stylelint-config-standard-scss",
+    "stylelint-config-html/astro",
     "stylelint-config-clean-order",
   ],
-  plugins: ["stylelint-scss"],
+  plugins: [
+    "stylelint-scss",
+    "stylelint-use-nesting",
+    "stylelint-high-performance-animation",
+  ],
   rules: {
     // recommended rules
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
+    "selector-class-pattern": null,
+    "csstools/use-nesting": "always",
+    "plugin/no-low-performance-animation-properties": true,
   },
 };
