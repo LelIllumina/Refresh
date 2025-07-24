59665048193ec742e2afc63d8f3578641a971f1e
Lel Illumina <dragonbilal938@outlook.com>
2025-07-24 22:04:22 +0500

    feat: add some stuff to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,24 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://lel.nekoweb.org",
-
   server: { host: true },
   output: "static",
 
+  trailingSlash: "always",
+
+  build: {
+    format: "preserve",
+  },
+
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
+
   integrations: [partytown(), mdx(), sitemap()],
+
+  experimental: {
+    contentIntellisense: true,
+    clientPrerender: true,
+  },
 });
