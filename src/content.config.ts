ceb3cbcb3ac34094fb96e0e4885e1e0dbdd17b63
Lel Illumina <dragonbilal938@outlook.com>
2025-08-09 23:07:36 +0500

    feat: basic blog list component

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,12 +5,15 @@ import { glob } from "astro/loaders";
 
 const blogs = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blogs" }),
-  schema: ({ image }) =>
+  schema: () =>
     rssSchema.extend({
       title: z.string(),
       description: z.string(),
       date: z.coerce.date(),
-      thumb: z.preprocess((val) => `src/assets/images/blog/${val}`, image()),
+      thumb: z.preprocess(
+        (val) => `/src/assets/images/blog/${val}`,
+        z.string(),
+      ),
       tags: z.array(z.string()),
       keywords: z.array(z.string()).optional(),
       draft: z.boolean(),
@@ -18,28 +21,19 @@ const blogs = defineCollection({
 });
 const guides = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/guides" }),
-  schema: ({ image }) =>
+  schema: () =>
     rssSchema.extend({
       title: z.string(),
       description: z.string(),
       date: z.coerce.date(),
-      thumb: z.preprocess((val) => `src/assets/images/blog/${val}`, image()),
+      thumb: z.preprocess(
+        (val) => `/src/assets/images/blog/${val}`,
+        z.string(),
+      ),
       tags: z.array(z.string()),
       keywords: z.array(z.string()).optional(),
       draft: z.boolean(),
     }),
 });
-const archive = defineCollection({
-  loader: glob({
-    pattern: "**/*.{md,mdx}",
-    base: "./src/content/archive",
-  }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    thumb: z.string().optional(),
-  }),
-});
 
-export const collections = { blogs, guides, archive };
+export const collections = { blogs, guides };
