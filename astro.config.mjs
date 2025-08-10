46ed2ea95c0ebcb55a195bdc4fa781cb05d01df6
Lel Illumina <dragonbilal938@outlook.com>
2025-08-11 04:05:29 +0500

    feat: add Aside component

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,14 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 import purgecss from "astro-purgecss";
-
 import playformCompress from "@playform/compress";
-
+import playformInline from "@playform/inline";
 import pageInsight from "astro-page-insight";
 import metaTags from "astro-meta-tags";
 
-import playformInline from "@playform/inline";
+import emoji from "remark-emoji";
+
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://lel.nekoweb.org",
@@ -38,11 +39,15 @@ export default defineConfig({
     partytown(),
     purgecss(),
     playformInline(),
-
     playformCompress(),
     pageInsight(),
     metaTags(),
+    icon(),
   ],
+
+  markdown: {
+    remarkPlugins: [[emoji, { accessible: true }]],
+  },
 
   experimental: {
     contentIntellisense: true,
